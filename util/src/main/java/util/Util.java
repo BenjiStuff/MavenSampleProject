97a4032ef7065217986f6cdf8a46f7b50ba3b654
myname <benjamin.vandervleuten@gxsoftware.com>
2018-03-14 12:39:02 +0100

    edi

--- util/src/main/java/util/Util.java
+++ util/src/main/java/util/Util.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Util {
 
-    //some doc
+    //some docu
     public static String join(String... elements) {
         return StringUtils.join(elements);
     }
